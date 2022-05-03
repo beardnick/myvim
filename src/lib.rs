cc0458204a470a63051cb5fb323ab9e1a15e96eb
qianz <1685437606@qq.com>
2022-05-03 08:09:20 +0000

    feat:simplify get tags api

--- src/lib.rs
+++ src/lib.rs
@@ -9,28 +9,11 @@ use crate::plug::github;
 
 //lua print(vim.inspect(require'myvim'.get_tags('neoclide/coc.nvim')))
 fn get_tags(lua: &Lua, repo: String) -> LuaResult<Vec<String>> {
-    let result = github::repo(repo.as_str());
-    let (owner, rep) = match result {
-        Ok((owner, rep)) => (owner, rep),
-        Err(e) => {
-            return Ok(Vec::new());
-        }
-    };
-    let tags = tokio_block!(github::get_tags(owner, rep));
-    let tags = match tags {
-        Ok(tags) => tags,
-        Err(e) => {
-            return Ok(Vec::new());
-        }
-    };
-    Ok(tags
-        .into_iter()
-        .map(|tag| tag.name)
-        .collect::<Vec<String>>())
+    let tags = tokio_block!(github::get_tags(repo.as_str()));
+    Ok(tags)
 }
 
 #[mlua::lua_module]
 fn myvim(lua: &Lua) -> LuaResult<LuaTable> {
-    let exports = lua.create_table()?;
     lua.create_table_from([("get_tags", lua.create_function(get_tags)?)])
 }
--- src/plug/github.rs
+++ src/plug/github.rs
@@ -1,38 +1,47 @@
 use octocrab::models;
-use octocrab::{Page, Error};
 use octocrab::models::repos::Tag;
+use octocrab::{Error, Page};
 
 use crate::error::PlugError;
 
-
-pub async fn get_tags(owner:&str,repo :&str) -> Result<Vec<Tag>,Error> {
+async fn get_repo_tags(owner: &str, repo: &str) -> Result<Vec<Tag>, Error> {
     let github = octocrab::instance();
-    let mut page = github
-        .repos(owner,repo)
-        .list_tags()
-        .send()
-        .await?;
-    let mut tags : Vec<Tag> = Vec::new();
+    let mut page = github.repos(owner, repo).list_tags().send().await?;
+    let mut tags: Vec<Tag> = Vec::new();
     loop {
-        for tag in &page { // 这里不能用&tag，因为&page是引用
+        for tag in &page {
+            // 这里不能用&tag，因为&page是引用
             tags.push(tag.clone());
         }
-        page = match github
-            .get_page::<Tag>(&page.next)
-            .await?
-        {
+        page = match github.get_page::<Tag>(&page.next).await? {
             Some(next_page) => next_page,
             None => break,
         };
     }
-        return Ok(tags);
+    return Ok(tags);
 }
 
-pub fn repo(plug:&str) ->  Result<(&str,&str),PlugError> {
+pub async fn get_tags(repo: &str) -> Vec<String> {
+    let (owner, repo) = match repository(repo) {
+        Ok((owner, repo)) => (owner, repo),
+        Err(_) => {
+            return vec![];
+        }
+    };
+    let tags = match get_repo_tags(owner, repo).await {
+        Ok(tags) => tags,
+        Err(_) => {
+            return vec![];
+        }
+    };
+    tags.into_iter().map(|tag| tag.name).collect()
+}
+
+pub fn repository(plug: &str) -> Result<(&str, &str), PlugError> {
     let mut plug = plug.split("/");
     let owner = plug.next().ok_or(PlugError::InvalidRepo)?;
     let repo = plug.next().ok_or(PlugError::InvalidRepo)?;
-    Ok((owner,repo))
+    Ok((owner, repo))
 }
 
 #[cfg(test)]
@@ -45,23 +54,22 @@ mod tests {
     #[tokio::main]
     #[test]
     async fn test_get_tags() {
-        let tags = get_tags("neoclide","coc.nvim")
+        let tags = get_repo_tags("neoclide", "coc.nvim")
             .await
             .expect("should not get error");
-        assert_ne!(tags.len(),0);
+        assert_ne!(tags.len(), 0);
     }
 
     #[test]
     fn repo_test() {
-        let (owner,rep) = repo("a/b").unwrap();
-        assert_eq!((owner,rep),("a","b"));
-        assert!(repo("ab").is_err());
+        let (owner, rep) = repository("a/b").unwrap();
+        assert_eq!((owner, rep), ("a", "b"));
+        assert!(repository("ab").is_err());
     }
 
     #[test]
-    fn test_get_tags_sync(){
-        let tags = tokio_block!( get_tags("neoclide","coc.nvim")).unwrap();
-        assert_ne!(tags.len(),0);
+    fn test_get_tags_sync() {
+        let tags = tokio_block!(get_repo_tags("neoclide", "coc.nvim")).unwrap();
+        assert_ne!(tags.len(), 0);
     }
-
 }

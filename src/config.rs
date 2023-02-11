741330cbb74ec96586573e47957248be467e8645
qianz <1685437606@qq.com>
2023-02-11 23:08:43 +0800

    feat:update deps

--- src/config.rs
+++ src/config.rs
@@ -1,19 +1,27 @@
 use serde::Deserialize;
-use nvim_oxi::{FromObject, Object, object,Result};
+use nvim_oxi::{lua, Object, Result};
+use nvim_oxi::conversion::{self, Error, FromObject, ToObject};
+use nvim_oxi::lua::ffi::lua_State;
+use nvim_oxi::lua::Poppable;
+use nvim_oxi::serde::Deserializer;
+
 
 #[derive(Debug, Default, Deserialize)]
 pub struct Config {
     pub log_dir: String,
 }
+
 impl FromObject for Config {
-    fn from_obj(obj: Object) -> Result<Self> {
-        Self::deserialize(object::Deserializer::new(obj))
+    fn from_object(object: Object) -> std::result::Result<Self, Error> {
+        Self::deserialize(Deserializer::new(object)).map_err(Into::into)
     }
 }
 
-// global mut is unsafe in rust
-// static mut conf: Config = Config::default();
+impl Poppable for Config {
+    unsafe fn pop(lua_state: *mut lua_State) -> std::result::Result<Self, nvim_oxi::lua::Error> {
+        let obj = Object::pop(lua_state)?;
+        Self::from_object(obj)
+            .map_err(lua::Error::pop_error_from_err::<Self, _>)
 
-//pub fn setup(c: Config) {
-//    conf = c;
-//}
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,7 @@ use log::{info, LevelFilter};
 use nvim_oxi::{
     self as oxi,
     api::{self, Window},
-    opts::*,
     print,
-    types::*,
     Dictionary,
     Function,
 };
@@ -31,7 +29,7 @@ fn get_tags(repo: String) -> oxi::Result<Vec<String>> {
 }
 
 fn setup(preference: &RefCell<Client>, conf: Config) -> oxi::Result<()> {
-    let mut client  = preference.borrow_mut();
+    let mut client = preference.borrow_mut();
     client.conf = conf;
     simple_logging::log_to_file(client.conf.log_dir.clone(), LevelFilter::Info);
     Ok(())
@@ -39,10 +37,9 @@ fn setup(preference: &RefCell<Client>, conf: Config) -> oxi::Result<()> {
 
 #[oxi::module]
 fn myvim() -> oxi::Result<Dictionary> {
-
     let mut client = RefCell::new(Client::default());
 
-    let pub_setup = move |conf: Config| { setup( &client, conf) };
+    let pub_setup = move |conf: Config| { setup(&client, conf) };
     Ok(Dictionary::from_iter([
         ("get_tags", Object::from(Function::from_fn(get_tags))),
         ("setup", Object::from(Function::from_fn(pub_setup))),
